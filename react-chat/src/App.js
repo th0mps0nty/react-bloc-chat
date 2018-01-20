c2c887c1c812f4495f5a93c9e24447e6322b44c4
Tyler Thompson <th0mps0n.ty@protonmail.com>
2018-01-20 00:25:43 -0500

    added firebase

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCDp9r_UHNEsAubNXz8A2gssuhRlmUyseI",
+    authDomain: "react-bloc-chat-451c0.firebaseapp.com",
+    databaseURL: "https://react-bloc-chat-451c0.firebaseio.com",
+    projectId: "react-bloc-chat-451c0",
+    storageBucket: "react-bloc-chat-451c0.appspot.com",
+    messagingSenderId: "4855665423"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
