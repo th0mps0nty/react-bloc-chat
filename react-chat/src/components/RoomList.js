a5980f2598d8a24183c97b8bb035b0950043129a
Tyler Thompson <th0mps0n.ty@protonmail.com>
2018-01-23 15:37:17 -0500

    added form for creating new chatrooms

--- react-chat/src/components/RoomList.js
+++ react-chat/src/components/RoomList.js
@@ -6,9 +6,12 @@ class RoomList extends Component {
     super(props);
 
     this.state = {
-      rooms: []
+      rooms: [],
+      submitForm: false,
+      newRoomName: ''
     };
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.createRoom = this.createRoom.bind(this);
   }
 
   componentDidMount() {
@@ -19,16 +22,39 @@ class RoomList extends Component {
     });
   }
 
+  toggleForm() {
+    if (this.state.submitForm === true) {
+      this.setState({submitForm: false});
+    } else {
+      this.setState({submitForm: true});
+    }
+  }
+
+  createRoom(e) {
+    e.preventDefault();
+    const newRoom = e.target.elements.newRoomName.value;
+    this.roomsRef.push({name: newRoom});
+    this.setState({submitForm: false});
+  }
+
   render() {
     return (
       <div className='roomlist'>
         <h1 className='title'>React Bloc Chat</h1>
+        <button className='new-room' onClick={this.toggleForm.bind(this)}>Create Room</button>
         <ul className='sidebar-list'>
           {
             this.state.rooms.map((room, index) =>
-            <li className='rooms' key={index}>{room.name}</li>
+              <li className='rooms' key={index}>{room.name}</li>
           )}
         </ul>
+        <form className={this.state.submitForm ? 'displayed' : 'hidden'} onSubmit={this.createRoom}>
+          <h2 className='form-title'>Create New Room</h2>
+          <h3 className='text-field-description'>Enter Room Name</h3>
+          <input type='text' id='new-room-name' name='newRoomName' />
+          <button className='cancel' onClick={this.toggleForm}>Cancel</button>
+          <button className='create-room' type='submit'>Create Room</button>
+        </form>
       </div>
     );
   }
